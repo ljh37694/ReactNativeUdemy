e6b0b657411fb589e2ec146a6b3d75c8ea22f8a1
i_junghoon <84302688+ljh37694@users.noreply.github.com>
2025-08-28 23:45:01 +0900

    flex 실습

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,26 +1,35 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text style={styles.textTest}>Kookmin</Text>
-      <Button title='Click me' onPress={() => { alert("Hello")}}></Button>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>
+          List of goals...
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 30,
   },
-  textTest: {
-    padding: 8,
-    borderColor: "black",
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  textInput: {
     borderWidth: 1,
-    borderRadius: 10,
-  }
+    borderColor: '#b6b6b6ff',
+    width: '80%',
+    padding: 3,
+    marginRight: 5
+  },
 });
