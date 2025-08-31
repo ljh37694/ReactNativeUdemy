8cb8a3d9c0c4173dfab7b997eaa650e3fb40ec02
i_junghoon <84302688+ljh37694@users.noreply.github.com>
2025-08-31 13:49:18 +0900

    andriod_ripple와 style로 스타일 추가

--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -4,22 +4,30 @@ export default function GoalItem(props) {
   const { data, onDeleteGoal } = props;
 
   return (
-    <Pressable onPress={onDeleteGoal.bind(this, data.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#ddd" }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+        onPress={onDeleteGoal.bind(this, data.id)}
+      >
         <Text style={styles.goalText}>{data.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
-    padding: 15,
     backgroundColor: "#a25dbfff",
     borderRadius: 10,
     marginBottom: 10,
   },
   goalText: {
     color: "#fff",
+    padding: 15,
   },
+  pressedItem: {
+    opacity: 0.5,
+    backgroundColor: '#77438dff',
+  }
 });
